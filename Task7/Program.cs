4db586dedd9d7932faca5baf43b536b78e56fc10
Andrei <kadrov2013@yandex.ru>
2022-09-09 19:20:08 +0300

    Check_Weekends

---
+++ Task7/Program.cs
@@ -0,0 +1,10 @@
+﻿//Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+Console.WriteLine("Введите номер дня недели: ");
+int numDay = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(CheckWeekends(numDay));
+
+bool CheckWeekends(int numDayCheck)
+{
+    return numDayCheck == 6 || numDayCheck == 7;
+}
