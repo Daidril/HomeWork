67573411dbfd70968cf4f835475af3da39a648ad
Andrei <kadrov2013@yandex.ru>
2022-09-09 19:15:27 +0300

    FindSecond_Digit

---
+++ Task5/Program.cs
@@ -0,0 +1,10 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.WriteLine("Введите трехзначное число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+FindSecondDigit(number1);
+
+void FindSecondDigit(int number1)
+{
+    Console.WriteLine(number1 % 100 / 10);
+}
