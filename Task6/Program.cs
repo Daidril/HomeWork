f6649214be228189fa07021081d4603597f84e12
Andrei <kadrov2013@yandex.ru>
2022-09-09 19:17:48 +0300

    Find_Third_Digit

---
+++ Task6/Program.cs
@@ -0,0 +1,22 @@
+﻿//Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.WriteLine("Ведите число: ");
+int firstnumber = Convert.ToInt32(Console.ReadLine());
+
+FindThirdDigit(firstnumber);
+
+void FindThirdDigit(int secondnumber)
+{
+    if(secondnumber < 100) 
+    {
+        Console.WriteLine("Третьей цифры нет");
+    }
+    else
+{
+    while (secondnumber >= 1000)
+    {
+        secondnumber = secondnumber / 10;
+    }
+    Console.WriteLine(secondnumber % 10);
+}
+
+}
